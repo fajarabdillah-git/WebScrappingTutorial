9042648f0a29407047bc565177f1f9b20a5bbfe3
Abdillah Faisal Nur Fajar <72332637+fajarabdillah-git@users.noreply.github.com>
2021-06-11 08:33:30 +0000

    grab all the Two starred product from a website as in video 120 and 121

--- main.py
+++ main.py
@@ -99,3 +99,66 @@ print(image)
 print("\n> Grab the image link add the 'https:' attribute to make the url be able to open on browser")
 image_link="https:" + image.select('img')[0]['src']
 print(image_link)
+
+print("\n+++++++++++++++++++++++++++++++++++++++++++++")
+print("Video 121. Web Scrapping - Book examples - Part One (play with web page)")
+print("+++++++++++++++++++++++++++++++++++++++++++++")
+
+print("\n==> Playing with page number on a website <==")
+web_url="https://books.toscrape.com/catalogue/page-{}.html"
+
+print("\nChoose the page number by modifying the web-url's format")
+page_num=1
+web_url_p1=web_url.format(page_num)
+
+res = requests.get(web_url_p1)
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+# uncomment the print command below if you want to see the html of the webpage
+# print(soup)
+
+print("\n+++++++++++++++++++++++++++++++++++++++++++++")
+print("Video 122. Web Scrapping - Book examples - Part One (Get all the products that have 2-stars score)")
+print("+++++++++++++++++++++++++++++++++++++++++++++")
+
+print("\n==> Find the pattern to get the 2-stars score books from the page 1 as an example <==")
+print("\n> Get the product's class")
+product=soup.select('.product_pod')
+print(product)
+
+print("\n> Get one product as an example to know the pattern")
+example=product[0]
+print(example)
+
+print("\n^^ From that html, we found a class named 'star-rating Three' as the product is three stared. So we could try to find the 2-star rating product with searching a class named 'star-rating Two'")
+
+print("\n==> Then find the pattern to get the product's title <==")
+print("\n> First get the anchor (<a>) element where the title belong")
+anchor=example.select('a')
+print(anchor)
+
+print("\n> There are 2 elements with <a> element, so take a look where the title in and choose it")
+title_elem=anchor[1]
+print(title_elem)
+
+print("\n> Get the title object")
+title=title_elem['title']
+print(title)
+
+print("\n==> We know how to get the two stared and the title, so try to get them from 1st page until the 50th <==")
+
+two_star_books=[]
+
+for i in range(1,51):
+  scrape_url=web_url.format(i)
+  res = requests.get(scrape_url)
+
+  soup = bs4.BeautifulSoup(res.text, 'lxml')
+  books = soup.select('.product_pod')
+
+  for book in books:
+    if len(book.select('.star-rating.Two')) != 0: # Check if the product's rating is 2
+    # if 'star-rating Two' in book:             # alternative checking condition
+      book_title=book.select('a')[1]['title'] # Get the book's title
+      two_star_books.append(book_title)
+
+print(two_star_books)
